4046e9310edffdf6396abbcac796aefbd2a025b1
bala-amypo <bala@amypo.in>
2025-12-18 16:06:39 +0000

    User commit

--- src/main/java/com/example/demo/service/Serviceimpl/VehicleServiceImpl.java
+++ src/main/java/com/example/demo/service/Serviceimpl/VehicleServiceImpl.java
@@ -1,4 +1,9 @@
 package com.example.demo.service.Serviceimpl;
 public class VehicleServiceImpl implements VehicleService{
-    @Autowired VehicleRepository 
+    @Autowired VehicleRepository vehiclee;
+    @Override
+    public  Vehicle createVehicle(Vehicle vehi){
+        return vehiclee.save(vehi);
+    }
+    
 }
