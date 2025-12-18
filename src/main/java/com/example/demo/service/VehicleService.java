9d7cf298c06e206c4dffe50bb0bb043fd75a9039
bala-amypo <bala@amypo.in>
2025-12-18 15:54:39 +0000

    User commit

--- src/main/java/com/example/demo/service/VehicleService.java
+++ src/main/java/com/example/demo/service/VehicleService.java
@@ -1,5 +1,11 @@
 package com.example.demo.service;
+import com.example.demo.entity.Vehicle;
+import java.util.List;
 public interface VehicleService{
     Vehicle postdata(Vehicle vehi);
-    
+    List<Vehicle>getAllData();
+    String DeleteData(int id);
+    Vehicle findData(int id);
+    Vehicle UpdateData(int id,Vehicle entity);
+
 }
