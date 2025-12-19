4309eef5144cc1f223951dbe1bb90f0005c10c4e
bala-amypo <bala@amypo.in>
2025-12-19 15:50:15 +0000

    User commit

--- src/main/java/com/example/demo/model/ServiceEntry.java
+++ src/main/java/com/example/demo/model/ServiceEntry.java
@@ -2,17 +2,19 @@ package com.example.demo.model;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Lob;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrePersist;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-import jakarta.validation.constraints.Size;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
@@ -22,24 +24,33 @@ import lombok.AllArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ServiceEntry {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @ManyToOne
+
+    @OneToOne
     @JoinColumn(name = "vehicle_id", nullable = false)
     private Vehicle vehicle;
-    @ManyToOne
+
+    @OneToOne
     @JoinColumn(name = "garage_id", nullable = false)
     private Garage garage;
+
     @NotBlank
     private String serviceType;
+
     @NotNull
     private LocalDate serviceDate;
+
     @NotNull
     @Positive
     private int odometerReading;
+
     private String description;
+
     private LocalDateTime recordedAt;
+
     @PrePersist
     public void Onrecord() {
         LocalDateTime now = LocalDateTime.now();
--- src/main/java/com/example/demo/model/ServicePart.java
+++ src/main/java/com/example/demo/model/ServicePart.java
@@ -1,4 +1,47 @@
 package com.example.demo.model;
-public class ServicePart{
-    
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.JoinColumn;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+
+import lombok.Data;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ServicePart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "service_entry_id", nullable = false)
+    private ServiceEntry serviceEntry;
+
+    @NotBlank
+    private String partName;
+
+    @NotNull
+    @Positive
+    private Integer quantity;
+
+    @NotNull
+    @Positive
+    private BigDecimal cost;
 }
