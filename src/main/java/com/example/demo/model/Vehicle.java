c02b06680076ed4d5e5af456346d0d915cf97112
bala-amypo <bala@amypo.in>
2025-12-19 15:14:34 +0000

    User commit

--- src/main/java/com/example/demo/model/Vehicle.java
+++ src/main/java/com/example/demo/model/Vehicle.java
@@ -1,5 +1,5 @@
-package com.example.demo.entity ;
-import java.util.Date;
+package com.example.demo.entity;
+import java.time.LocalDateTime;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -8,21 +8,23 @@ import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
-
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Studententity{
+public class Vehicle {
+
     @Id
-    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+    @NotNull
+    @Column(name="vin",unique=true)
     private String vin;
     private String make;
-    private String model; 
+    private String model;
     private int year;
-    private Long ownerld;
-    private Boolean active;         
-    private LocalTimeDate created;
-
+    @NotNull
+    private Long ownerId;
+    private Boolean active;
+    private LocalDateTime created;
 }
