0cbb4a37129e68d1127f1c6b52126b7578e5494c
bala-amypo <bala@amypo.in>
2025-12-19 15:23:17 +0000

    User commit

--- src/main/java/com/example/demo/model/Vehicle.java
+++ src/main/java/com/example/demo/model/Vehicle.java
@@ -1,12 +1,16 @@
 package com.example.demo.entity;
+
 import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.PrePersist;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
 import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
@@ -18,26 +22,26 @@ public class Vehicle {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
     @NotNull
-    @Column(name="vin",unique=true)
+    @Column(name = "vin", unique = true)
     private String vin;
+
     private String make;
     private String model;
     private int year;
+
     @NotNull
-    private Long ownerId;
+    private long ownerId;
+
     private Boolean active;
     private LocalDateTime created;
+
     @PrePersist
-   
-    public void Oncreate(){
-    LocalDateTime now= LocalDateTime.now();
-    if(this.created==null){
-
-    this.created=now;
+    public void Oncreate() {
+        LocalDateTime now = LocalDateTime.now();
+        if (this.created == null) {
+            this.created = now;
+        }
     }
-    
-    }
-    
-
 }
