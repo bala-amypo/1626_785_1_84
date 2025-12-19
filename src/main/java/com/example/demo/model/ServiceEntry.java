801fe7a833c39038b6928df958d9ce9dcf74696a
bala-amypo <bala@amypo.in>
2025-12-19 16:08:16 +0000

    User commit

--- src/main/java/com/example/demo/model/ServiceEntry.java
+++ src/main/java/com/example/demo/model/ServiceEntry.java
@@ -1,8 +1,6 @@
 package com.example.demo.model;
-
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
@@ -10,11 +8,9 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrePersist;
-
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
-
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
--- src/main/java/com/example/demo/model/Vehicle.java
+++ src/main/java/com/example/demo/model/Vehicle.java
@@ -1,7 +1,5 @@
 package com.example.demo.entity;
-
 import java.time.LocalDateTime;
-
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -12,31 +10,24 @@ import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Vehicle {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
-
     @NotNull
     @Column(name = "vin", unique = true)
     private String vin;
-
     private String make;
     private String model;
     private int year;
-
     @NotNull
     private long ownerId;
-
     private Boolean active;
     private LocalDateTime created;
-
     @PrePersist
     public void Oncreate() {
         LocalDateTime now = LocalDateTime.now();
