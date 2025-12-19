eeecd1d33c75adcf42cdb99a1b1d40d9b43f6d5c
bala-amypo <bala@amypo.in>
2025-12-19 15:38:16 +0000

    User commit

--- src/main/java/com/example/demo/model/Garage.java
+++ src/main/java/com/example/demo/model/Garage.java
@@ -1,24 +1,15 @@
 package com.example.demo.model;
-
-import java.time.LocalDateTime;
-
 import jakarta.persistence.Entity;
-
 import jakarta.persistence.Id;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Column;
 import jakarta.persistence.OneToMany;
-
-import org.hibernate.annotations.CreationTimestamp;
-
 import jakarta.validation.constraints.NotBlank;
-
 import lombok.Data;
 import lombok.Builder;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
-
 import java.util.List;
 
 @Entity
@@ -27,23 +18,15 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Garage {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
     @NotBlank
     @Column(name="garageName",unique = true)
     private String garageName;
-
     @NotBlank
-    private String location;
-
-    private Boolean active = true;
-
-    @CreationTimestamp
-    private LocalDateTime createdAt;
-
-    @OneToMany(mappedBy = "garage")
-    private List<ServiceEntry> serviceEntries;
+    private String address;
+    private String contactNumber;
+    private Boolean active;
+   
 }
