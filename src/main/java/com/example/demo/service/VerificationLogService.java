a6217fb686d029b2822bec34104843d583bdd2c8
bala-amypo <bala@amypo.in>
2025-12-20 15:55:41 +0000

    User commit

--- src/main/java/com/example/demo/service/VerificationLogService.java
+++ src/main/java/com/example/demo/service/VerificationLogService.java
@@ -1,4 +1,6 @@
-// package com.example.demo.service;
-// public interface VerificationLogService{
+package com.example.demo.service;
+public interface VerificationLogService{
+    VerificationLog createVerificationLog(VerificationLog log);
     
-// }
+
+}
