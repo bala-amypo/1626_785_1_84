7a4fceb8dd6d29c3ce810dd8d36de5c8deaf41bc
bala-amypo <bala@amypo.in>
2025-12-20 14:20:12 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -2,6 +2,6 @@ package com.example.demo.service;
 import java.util.List;
 import com.example.demo.model.User;
 public interface UserService{
-     UserService createGarage(UserService reg);
+     UserService createUserService(UserService reg);
      List<UserService>getAllUser();
 }
