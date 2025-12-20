8202d37d20e01a7638dbec4d0f60ac2f13f7a2a8
bala-amypo <bala@amypo.in>
2025-12-20 05:54:52 +0000

    User commit

--- src/main/java/com/example/demo/repository/VehicleRepository.java
+++ src/main/java/com/example/demo/repository/VehicleRepository.java
@@ -1,7 +1,7 @@
 package com.example.demo.repository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.entity.Vehicle;
+import com.example.demo.entity.Vehicle;
 @Repository
 public interface VehicleRepository extends JpaRepository <Vehicle,Long>{
     
