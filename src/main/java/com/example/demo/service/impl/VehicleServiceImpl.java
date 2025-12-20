6c387b263beb62dfebdad47a80a2ab8d1078e6d5
bala-amypo <bala@amypo.in>
2025-12-20 08:26:12 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/VehicleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/VehicleServiceImpl.java
@@ -1,45 +1,38 @@
-package com.example.demo.service.impl;
-import org.springframework.stereotype.Service;
-import com.example.demo.service.VehicleService;
-import  com.example.demo.entity.Vehicle;
-import  com.example.demo.repository.VehicleRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import java.util.List;
+@Service
+public class VehicleServiceImpl implements VehicleService {
 
-public class VehicleServiceImpl implements VehicleService{
     private final VehicleRepository v;
-    public VehicleServiceImpl(VehicleRepository v){
-        this.v=v;
+
+    public VehicleServiceImpl(VehicleRepository v) {
+        this.v = v;
     }
+
     @Override
-    public  Vehicle createVehicle(Vehicle vehicle){
+    public Vehicle createVehicle(Vehicle vehicle) {
         return v.save(vehicle);
-        
     }
+
     @Override
-    public Vehicle getVehicleById(Long id){
-      return v.findById(id).orElse(null);
-     }
+    public Vehicle getVehicleById(Long id) {
+        return v.findById(id).orElse(null);
+    }
+
     @Override
     public Vehicle getVehicleByVin(String vin) {
-      return v.findById(vin).orElse(null);
-     }
+        return v.findByVin(vin);   
+    }
+
     @Override
-    public  List <Vehicle> getVehicleByOwner(Long ownerId){
+    public List<Vehicle> getVehicleByOwner(Long ownerId) {
         return v.findAll();
     }
+
     @Override
-    public void deactivateVehicle(Long id){
-         Vehicle vehicle=v.findById(id).orElse(null);
-         if(vehicle!=null){
-            vehicle.setIsactive(false);
+    public void deactivateVehicle(Long id) {
+        Vehicle vehicle = v.findById(id).orElse(null);
+        if (vehicle != null) {
+            vehicle.setActive(false);
             v.save(vehicle);
-         }
+        }
     }
-
 }
-
-
-
-
-
