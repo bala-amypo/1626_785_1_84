ad87e8718573dbd83b90f90bbcf33a5d0f8487b3
bala-amypo <bala@amypo.in>
2025-12-20 11:20:12 +0000

    User commit

--- src/main/java/com/example/demo/controller/ServiceEntryController.java
+++ src/main/java/com/example/demo/controller/ServiceEntryController.java
@@ -6,10 +6,10 @@ import com.example.demo.service.ServiceEntryService;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @RestController
-@RequestMapping("/service-entries")
+
 public class ServiceEntryController {
 
     private final ServiceEntryService serr;
@@ -18,7 +18,7 @@ public class ServiceEntryController {
         this.serr = serr;
     }
 
-    @PostMapping
+    @PostMapping(/POSTTT)
     public ServiceEntry createServiceEntry(@RequestBody ServiceEntry entry) {
         return serr.createServiceEntry(entry);
     }
