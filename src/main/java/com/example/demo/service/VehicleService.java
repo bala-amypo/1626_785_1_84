1a63e82d01cd09f0e54e61de4012e4dacca3e5a3
bala-amypo <bala@amypo.in>
2025-12-20 04:45:51 +0000

    User commit

--- src/main/java/com/example/demo/service/VehicleService.java
+++ src/main/java/com/example/demo/service/VehicleService.java
@@ -3,6 +3,6 @@ import com.example.demo.entity.Vehicle;
 import java.util.List;
 public interface VehicleService{
     Vehicle createVehicle(Vehicle vehicle);
-    List<Vehicle>getVehicle();
+    List<Vehicle>getVehicleByI();
     
 }
