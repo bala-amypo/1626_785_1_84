88439d9ef570c6d8c1ee7d900302c58f2b4341f7
bala-amypo <bala@amypo.in>
2025-12-20 09:17:11 +0000

    User commit

--- src/main/java/com/example/demo/model/Garage.java
+++ src/main/java/com/example/demo/model/Garage.java
@@ -1,30 +1,30 @@
-// package com.example.demo.model;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.Column;
-// import jakarta.validation.constraints.NotBlank;
-// import lombok.Data;
-// import lombok.Builder;
-// import lombok.NoArgsConstructor;
-// import lombok.AllArgsConstructor;
-// import java.util.List;
-// @Entity
-// @Data
-// @Builder
-// @NoArgsConstructor
-// @AllArgsConstructor
-// public class Garage {
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
-//     @NotBlank
-//     @Column(name="garageName",unique = true)
-//     private String garageName;
-//     @NotBlank
-//     private String address;
-//     private String contactNumber;
-//     private Boolean active;
+package com.example.demo.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import java.util.List;
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Garage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotBlank
+    @Column(name="garageName",unique = true)
+    private String garageName;
+    @NotBlank
+    private String address;
+    private String contactNumber;
+    private Boolean active;
    
-// }
+}
