9238c0ee50d333921c594744def803650f940d2c
bala-amypo <bala@amypo.in>
2025-12-20 12:08:11 +0000

    User commit

--- src/main/java/com/example/demo/controller/ServiceEntryController.java
+++ src/main/java/com/example/demo/controller/ServiceEntryController.java
@@ -1,7 +1,5 @@
 package com.example.demo.controller;
-
 import java.util.List;
-
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
