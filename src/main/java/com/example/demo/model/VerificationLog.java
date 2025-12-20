573fff7c700d433f148820f564a5f5556162b611
bala-amypo <bala@amypo.in>
2025-12-20 15:49:42 +0000

    User commit

--- src/main/java/com/example/demo/model/VerificationLog.java
+++ src/main/java/com/example/demo/model/VerificationLog.java
@@ -1,44 +1,44 @@
-package com.example.demo.model;
-import java.time.LocalDateTime;
-import jakarta.persistence.PrePersist;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.Column;
-import jakarta.validation.constraints.NotBlank;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import lombok.AllArgsConstructor;
+// package com.example.demo.model;
+// import java.time.LocalDateTime;
+// import jakarta.persistence.PrePersist;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Id;
+// import jakarta.persistence.GeneratedValue;
+// import jakarta.persistence.GenerationType;
+// import jakarta.persistence.OneToOne;
+// import jakarta.persistence.JoinColumn;
+// import jakarta.persistence.Column;
+// import jakarta.validation.constraints.NotBlank;
+// import lombok.Data;
+// import lombok.NoArgsConstructor;
+// import lombok.AllArgsConstructor;
 
-@Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class VerificationLog {
+// @Entity
+// @Data
+// @NoArgsConstructor
+// @AllArgsConstructor
+// public class VerificationLog {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
 
-    @OneToOne
-    @JoinColumn(name = "service_entry_id", nullable = false)
-    private ServiceEntry serviceEntry;
+//     @OneToOne
+//     @JoinColumn(name = "service_entry_id", nullable = false)
+//     private ServiceEntry serviceEntry;
 
-    private LocalDateTime verifiedAt;
-    @PrePersist
-    public void Onverify() {
-        LocalDateTime now = LocalDateTime.now();
-        if (this.verifiedAt == null) {
-            this.verifiedAt = now;
-        }
-    }
+//     private LocalDateTime verifiedAt;
+//     @PrePersist
+//     public void Onverify() {
+//         LocalDateTime now = LocalDateTime.now();
+//         if (this.verifiedAt == null) {
+//             this.verifiedAt = now;
+//         }
+//     }
 
-    @NotNull
-     @Column(nullable = false)
-    private Boolean verifiedBySystem=true;
+//     @NotNull
+//      @Column(nullable = false)
+//     private Boolean verifiedBySystem=true;
 
-    private String notes;
-}
+//     private String notes;
+// }
