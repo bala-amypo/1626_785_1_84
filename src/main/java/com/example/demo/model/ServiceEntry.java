6414ca8ddf445b7659b9f433ff7925f2c68cc222
bala-amypo <bala@amypo.in>
2025-12-22 07:07:47 +0000

    User commit

--- src/main/java/com/example/demo/model/ServiceEntry.java
+++ src/main/java/com/example/demo/model/ServiceEntry.java
@@ -15,7 +15,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 
-import com.example.demo.entity.Vehicle;
+import com.example.demo.model.Vehicle;
 
 @Entity
 @Data
