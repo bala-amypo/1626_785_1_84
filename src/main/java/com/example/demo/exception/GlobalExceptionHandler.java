949e186fd32c1f9cea7e9aabbe6ca6737cf4c0d8
bala-amypo <bala@amypo.in>
2025-12-22 12:41:21 +0000

    User commit

---
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -0,0 +1,13 @@
+package com.example.demo.exception;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler{
+    @ExceptionHandler(VehicleNotFoundException.class)
+    public ResponseEntity<String> handleVehicleNotFoundException(VehicleNotFoundException ex){
+        return new ResponseEntity<String>(ex.getMessage());
+    }
+}
