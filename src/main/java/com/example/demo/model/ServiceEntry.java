55dd88447d84847a82eae1610bef57c165797961
bala-amypo <bala@amypo.in>
2025-12-22 12:29:21 +0000

    User commit

--- src/main/java/com/example/demo/model/ServiceEntry.java
+++ src/main/java/com/example/demo/model/ServiceEntry.java
@@ -1,76 +1,15 @@
-// package com.example.demo.model;
-// import java.time.LocalDate;
-// import java.time.LocalDateTime;
-// import jakarta.persistence.Entity;
-// import jakarta.persistence.Id;
-// import jakarta.persistence.GeneratedValue;
-// import jakarta.persistence.GenerationType;
-// import jakarta.persistence.JoinColumn;
-// import jakarta.persistence.ManyToOne;
-// import jakarta.persistence.PrePersist;
-// import jakarta.validation.constraints.NotBlank;
-// import jakarta.validation.constraints.NotNull;
-// import jakarta.validation.constraints.Positive;
-// import lombok.Data;
-// import lombok.NoArgsConstructor;
-// import lombok.AllArgsConstructor;
-// import com.example.demo.model.Vehicle;
 
-// @Entity
-// @Data
-// @NoArgsConstructor
-// @AllArgsConstructor
-// public class ServiceEntry {
-
-//     @Id
-//     @GeneratedValue(strategy = GenerationType.IDENTITY)
-//     private Long id;
-
-//  @ManyToOne
-//  @JoinColumn(name = "vehicle_id", nullable = false)
-// private Vehicle vehicle;
-
-// @ManyToOne
-// @JoinColumn(name = "garage_id", nullable = false)
-// private Garage garage;
-
-// @NotBlank
-// @NotNull
-// private String serviceType;
-
-// @NotNull
-// private LocalDate serviceDate;
-
-// @NotNull
-// @Positive
-// private int odometerReading;
-
-// @NotNull
-// private String description;
-
-// @NotNull
-// private LocalDateTime recordedAt;
-
-//     @PrePersist
-//     public void Onrecord() {
-//         if (this.recordedAt == null) {
-//             this.recordedAt = LocalDateTime.now();
-//         }
-//     }
-// }
 package com.example.demo.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Column;
-import jakarta.validation.constraints.NotBlank;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
+
 import lombok.Data;
-import lombok.Builder;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
-import java.util.List;
 
 import com.example.demo.entity.Vehicle;  
 import com.example.demo.model.Garage;   
