42077d0c9e9cd9a1b05fc0d1ebb3647f2bcc94e7
bala-amypo <bala@amypo.in>
2025-12-25 11:30:29 +0000

    User commit

--- src/main/java/com/example/demo/controller/VehicleController.java
+++ src/main/java/com/example/demo/controller/VehicleController.java
@@ -1,46 +1,47 @@
 package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
-import com.example.demo.service.VehicleService;
 import com.example.demo.entity.Vehicle;
+import com.example.demo.service.VehicleService;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/vehicles")
 public class VehicleController {
 
-    private final VehicleService service;
+    @Autowired
+    private VehicleService ser;
 
-    public VehicleController(VehicleService service) {
-        this.service = service;
-    }
-
-    @PostMapping
+    @PostMapping("/vehicles")
     public Vehicle createVehicle(@RequestBody Vehicle vehicle) {
-        return service.createVehicle(vehicle);
+        return ser.createVehicle(vehicle);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/vehicles/{id}")
     public Vehicle getVehicleById(@PathVariable Long id) {
-        return service.getVehicleById(id);
+        return ser.getVehicleById(id);
     }
 
-    @GetMapping("/vin/{vin}")
+    @GetMapping("/vehicles/vin/{vin}")
     public Vehicle getVehicleByVin(@PathVariable String vin) {
-        return service.getVehicleByVin(vin);
+        return ser.getVehicleByVin(vin);
     }
 
-    @GetMapping("/owner/{ownerId}")
+    @GetMapping("/vehicles/owner/{ownerId}")
     public List<Vehicle> getVehicleByOwner(@PathVariable Long ownerId) {
-        return service.getVehicleByOwner(ownerId);
+        return ser.getVehicleByOwner(ownerId);
+    }
+
+    @PutMapping("/vehicles/deactivate/{id}")
+    public String deactivateVehicle(@PathVariable Long id) {
+        ser.deactivateVehicle(id);
+        return "Vehicle deactivated successfully";
     }
 }
