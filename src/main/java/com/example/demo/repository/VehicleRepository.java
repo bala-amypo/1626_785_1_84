b118908b92831b23e4f3d68da101c492b5044f4e
bala-amypo <bala@amypo.in>
2025-12-25 14:51:11 +0000

    User commit

--- src/main/java/com/example/demo/repository/VehicleRepository.java
+++ src/main/java/com/example/demo/repository/VehicleRepository.java
@@ -1,15 +1,31 @@
+// package com.example.demo.repository;
+
+// import com.example.demo.model.Vehicle;
+// import java.util.List;
+// import java.util.Optional;
+// import org.springframework.data.jpa.repository.JpaRepository;
+// import org.springframework.stereotype.Repository;
+
+// @Repository
+// public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+
+//     Optional<Vehicle> findByVin(String vin);
+
+//     List<Vehicle> findByOwnerId(Long ownerId);
+// }
+
 package com.example.demo.repository;
 
 import com.example.demo.model.Vehicle;
 import java.util.List;
-import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 
-    Optional<Vehicle> findByVin(String vin);
+
+    Vehicle findByVin(String vin);
 
     List<Vehicle> findByOwnerId(Long ownerId);
 }
