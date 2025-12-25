b6f711a8eadcf86afff4727d759863066adc8b6e
bala-amypo <bala@amypo.in>
2025-12-25 10:15:12 +0000

    User commit

--- src/main/java/com/example/demo/repository/GarageRepository.java
+++ src/main/java/com/example/demo/repository/GarageRepository.java
@@ -1,9 +1,10 @@
-// package com.example.demo.repositiry;
+package com.example.demo.repository;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.demo.model.Garage;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-// import com.example.demo.model.Garage;
+import java.util.Optional;
 
-// public interface GarageRepository extends JpaRepository <Garage,Long>{
-    
-// }
+public interface GarageRepository extends JpaRepository<Garage, Long> {
+    Optional<Garage> findByGarageName(String garageName);
+}
