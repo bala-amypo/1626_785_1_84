0c2c3bc414acdfa51da8cac07341aa6b933f5350
bala-amypo <bala@amypo.in>
2025-12-25 12:30:11 +0000

    User commit

--- src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/example/demo/exception/GlobalExceptionHandler.java
@@ -52,4 +52,11 @@ public class GlobalExceptionHandler {
     public ResponseEntity<String> handleNotFound(EntityNotFoundException ex) {
         return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
     }
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleOtherExceptions(Exception ex) {
+        return new ResponseEntity<>(
+                "Internal server error",
+                HttpStatus.INTERNAL_SERVER_ERROR
+        );
+    }
 }
