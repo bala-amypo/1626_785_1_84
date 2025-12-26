0537f877ac0772270a0e8568466165b3ed72f7c1
bala-amypo <bala@amypo.in>
2025-12-26 08:37:30 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import io.swagger.v3.oas.annotations.Operation;
@@ -13,6 +15,10 @@ import io.swagger.v3.oas.annotations.Operation;
     DataSourceAutoConfiguration.class, 
     HibernateJpaAutoConfiguration.class,
     JpaRepositoriesAutoConfiguration.class
+})
+@ComponentScan(excludeFilters = {
+    @ComponentScan.Filter(type = FilterType.REGEX, pattern = "com\\.example\\.demo\\.service\\.impl\\..*"),
+    @ComponentScan.Filter(type = FilterType.REGEX, pattern = "com\\.example\\.demo\\.repository\\..*")
 })
 public class DemoApplication {
 
